2e31e4a4fb77c933c1e1df89d885b79daa36d4ad
IvanBuzuk <ivan.buzuk2000@gmail.com>
2017-05-28 23:02:37 +0200

    update

--- 10dz.cpp
+++ 10dz.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
+#include<fstream>
+#include<string>
 using namespace std;
 
 int main()
 {
+    int NazivPredmeta;
+    cout <<"unesite predmete";
+    cin >> NazivPredmeta;
+    string predmeti;
+    fstream textfile;
+    textfile.open("predmeti.txt");
+
     return 0;
 }
